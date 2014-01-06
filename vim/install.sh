327118083dea60b5b1926cee482caa511bb27e88
ivannotes <miracle.ivanlee@gmail.com>
2014-01-06 22:57:37 +0800

    Add gocode for golang autocomplete

--- vim/install.sh
+++ vim/install.sh
@@ -9,3 +9,6 @@ sudo pip install jedi
 
 go get -v code.google.com/p/rog-go/exp/cmd/godef
 go install -v code.google.com/p/rog-go/exp/cmd/godef
+go get -u github.com/nsf/gocode
+
+sh $GOPATH/src/github.com/nsf/gocode/vim/pathogen_update.sh
